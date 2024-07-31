bd618e24d02cb586150eabfdeb3e5c7fcf98f6e2
Luis Guillen <luisguile495@gmail.com>
2024-07-31 11:41:13 -0400

    actualización seguir en casa

---
+++ 5_Condicionales.py
@@ -0,0 +1,31 @@
+# 5_Condicionales
+
+# Toda sentencia de condicional es el if
+# Se debe resptar las sangrias aunque se pueden escribir las instrucciones al lado
+
+a = input('Ingrese un valor númerico sin decimales: ')
+
+# if(2+2 == 4):
+#     print(1)
+
+if(2-2 == 0):
+    print(f'Es igual a = {a}')
+
+# if con else: solo se ejuecuta si el condicional anterior es false
+a = int(input('Ingrese el número para saber si es par o no: '))
+if(a%2 == 0): # a%2 Es saber si el cociente es 0 al final de la división
+    print(f'El valor que ingresaste ({a}) es par')
+else:
+    print('Es impar')
+
+# Elif 
+
+a = input('Ingrese una letra: ')
+if (a == 't'):
+    print('Es la letra t, el valor que ingresaste')
+elif (a == 's'):
+    print('Es la letra S, el valor que agregaste')
+elif (a == 'j'):
+    print('Es la letra J, el valor que agregaste')
+else:
+    print('No es la letra que se busca')
---
+++ 5_ciclos.py
@@ -0,0 +1,25 @@
+# 5_ciclos
+
+# Existen dos ciclos while y for , se hacen para ahorrar tiempo
+
+# i = 1
+# while(1<=10):
+#     print(i, end='  ') # end= '  ' marca la distancia entre lo números en el print
+#     i += 1
+
+# Ejemplo 2
+
+# range(5) ---> 0,1,2,3,4 (Empieza a contar desde el 0 hasta el 4)
+# range(1,6) ---> 1,2,3,4,5 (Emepiza a contar desde el 1 hasta el 5, el utltimo rango es el 5 )
+# range(1, 11, 2) ---> 1,3,5,7,9  (3 argumnentos generan un )
+
+# Ejemplo for 
+
+for i in range (1,11):
+    print(i, end='   ')
+
+# Ejemplo 4 imprimir variables en ciclo for
+a = 'abcdefg'
+
+for i in a: # donde i (variable que acumula el ciclo) dentro de a (variable que contiene el str):
+    print(i, end='  ') # Imprime i , con está separación
